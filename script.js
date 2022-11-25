94fa70190de82d81291a9aac1654eed8dca5f27f
rishika570 <rishikasingh570@gmail.com>
2022-11-25 22:53:18 +0530

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// const bt1=document.querySelector('#b1');
+// const bt2=document.querySelector('#b2');
+// const bt3=document.querySelector('#b3');
+// const bt4=document.querySelector('#b4');
+// const bt5=document.querySelector('#b5');
+
+// bt1.addEventListener('click',() => {
+//     document.documentElement.style.setProperty('blue');
+// })
